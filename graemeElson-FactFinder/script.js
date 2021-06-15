4e07a3bd3243cc51e2b8429af52ad5d3ba355efe
Graemeschool <85921657+Graemeschool@users.noreply.github.com>
2021-06-14 22:32:27 -0700

    Add files via upload

---
+++ graemeElson-FactFinder/script.js
@@ -0,0 +1,29 @@
+document.querySelector('#b').innerHTML = "<h1><b>Welcome to my fun facts page!</b></h1>";
+
+
+
+document.querySelector('#a').f1.addEventListener("click",fp1);
+function fp1(){
+document.querySelector('#b').innerHTML = "Oranges aren't naturally occurring fruits!<br><p>Oranges may be an iconic fruit, but they are not a naturally occurring one, as The Telegraph points out. In fact, oranges are a hybrid of tangerines and pomelos, also known as Chinese grapefruit, and they were originally green—not, well, orange. Oranges are a subtropical fruit, but now that they exist in more temperate climates, they lose their chlorophyll-induced green and become their more familiar color when the weather warms up.)</p>";
+}
+
+
+document.querySelector('#a').f2.addEventListener("click",fp2);
+function fp2(){
+document.querySelector('#b').innerHTML = "Some cats are actually allergic to humans!<br><img src='images/cat.jpg'><br><p>Though it's uncommon—since humans bathe more than your typical animal, and don't shed as much hair or skin—some animals can still be allergic to humans, according to Popular Science. (However, it's more often because of the perfume or cologne we wear, or the soap we use.)</p>";
+}
+
+document.querySelector('#a').f3.addEventListener("click",fp3);
+function fp3(){
+document.querySelector('#b').innerHTML = "High heels were originally worn by men!<br><p>In the 10th century, men in Europe adopted the now-gendered fashion choice of heels to make it easier to ride their horses: Adding heels to their boots made it easier to stay in their stirrups. As Slate explains, The Persian cavalry wore inch-high heels, and the trend spread to Europe. Since they showed that the wearer owned and maintained horses, high heels became associated with the upper class.)</p>";
+}
+
+document.querySelector('#a').f4.addEventListener("click",fp4);
+function fp4(){
+document.querySelector('#b').innerHTML = "Sea otters hold hands while they sleep!<br><p>This ridiculously cute behavior is an effort to avoid floating away from their partner during sleep. Sometimes they hold hands in groups, producing a raft.</p>";
+}
+
+document.querySelector('#a').f5.addEventListener("click",fp5);
+function fp5(){
+document.querySelector('#b').innerHTML = "The moon is (slowly) slowing the Earth's rotation!<br><p>Every one hundred years, the moon adds approximately 1.4 milliseconds to a day. While this may be minuscule, it does add up: When dinosaurs roamed the planet, days were 23 hours long, according to NASA.</p>";
+}
